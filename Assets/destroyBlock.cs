12fac3059c639587aefd5b5cf86ef04271c2afef
dan <danbickford007@yahoo.com>
2015-10-07 23:50:50 -0400

    first working world

---
+++ Assets/destroyBlock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyBlock : MonoBehaviour {
+
+	GameObject clickedItem = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+
+//	private void OnMouseEnter() {
+//		Debug.Log ("Mousing.....");
+//		Debug.Log ("Mousing the block " + gameObject.name);
+//		clickedItem = gameObject;
+//	}
+
+
+
+	void OnMouseDown(){
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("Removing block -> " + clickedItem.name);
+			Destroy (this.gameObject);
+		}
+
+	}   
+}
